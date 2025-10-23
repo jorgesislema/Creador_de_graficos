3545047dd2c66163c6f289e1cda988f8fe064c63
jorgesislema <79588110+jorgesislema@users.noreply.github.com>
2025-10-23 08:38:19 -0500

    |

--- chart_maker/exporters/powerbi_pbiviz/exporter.py
+++
@@ -1,8 +0,0 @@
-# exporter.py
-# Exportador para Power BI pbiviz
-from ..base import IExporter
-
-class PowerBIPbivizExporter(IExporter):
-    def export(self, spec, output_path: str):
-        # Lógica para exportar a pbiviz usando plantillas
-        pass
